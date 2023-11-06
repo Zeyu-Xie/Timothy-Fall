7738c07c922671b72ddc171769ca3c3b84aa27a5
Acan <xie.zeyu20@gmail.com>
2023-11-06 17:34:45 +0800

    .

--- gameProcess.js
+++ gameProcess.js
@@ -1,11 +1,13 @@
 const randomInt = require("./randomInt")
 
+const alpha = "abcdefghijklmnopqrstuvwxyz"
+
 const gameProcess = (game, height, width) => {
     for(let i=height-1; i>=1;i--) {
         game[i] = game[i-1]
     }
     const str = new Array(width).fill(" ")
-    str[randomInt(0, width)] = (randomInt(0, 4) == 0) ? "b": " "
+    str[randomInt(0, width)] = (randomInt(0, 4) == 0) ? alpha[randomInt(0,26)]: " "
     game[0]=str.join("")
     return game
 }
