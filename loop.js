f4dfcdfbc7dc545963f669b6df7b864f9990a99d
acanxie <xie.zeyu20@gmail.com>
2023-11-06 19:26:49 +0800

    .

--- loop.js
+++ loop.js
@@ -1,13 +1,33 @@
-const gamePrint = require("./gamePrint");
-const gameProcess = require("./gameProcess");
+const gamePrint = require("./gamePrint")
+const gameProcess = require("./gameProcess")
+const keypress = require('keypress')
+
+process.stdin.setRawMode(true);
 
 const loop = (game, height, width) => {
     setTimeout(() => {
+
+        process.stdin.removeAllListeners('keypress')
+
         console.clear()
         gamePrint(game, height, width)
+
+        keypress(process.stdin)
+
+        process.stdin.on('keypress', (ch, key) => {
+            if (key && key.name === 'a') {
+                console.log('你按下了 "a" 键！');
+            }
+            process.stdin.resume();
+        });
+
+        
+        
+
+
         gameProcess(game, height, width)
         loop(game, height, width)
-    }, 200);
+    }, 2000);
 }
 
 module.exports = loop
